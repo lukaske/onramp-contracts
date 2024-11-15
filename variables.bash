c125a3fcad99395487c4bacea29b65e00d26d9a6
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-11-15 17:57:59 +0100

    Create variables.bash

---
+++ variables.bash
@@ -0,0 +1,7 @@
+export ONRAMP_CODE_PATH=$(pwd)
+export LOTUS_EXEC_PATH=/usr/local/bin/
+export BOOST_EXEC_PATH=/usr/local/bin/
+export XCHAIN_KEY_PATH=/home/ubuntu/onramp-contracts/xchain_key.json/UTC--2024-11-15T15-06-23.171305364Z--ac84493d334b5d75cded56b5b1ea0eb6bfca8934
+export XCHAIN_PASSPHRASE=jabuk123
+export XCHAIN_ETH_API="http://127.0.0.1:1234/rpc/v1"
+export MINER_ADDRESS=t01013
